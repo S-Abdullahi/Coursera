7a5affac9f2a41e8485770445ff1cee388ba33a1
S-Abdullahi <salabdullahi33@gmail.com>
2022-02-26 18:12:21 +0100

    our meal

--- mod2/main.js
+++ mod2/main.js
@@ -6,12 +6,15 @@ document.querySelector('#berbecue').addEventListener('click', berbecue)
 
 function chicken(){
     document.querySelector('body').style.backgroundColor = 'lightcyan'
+    document.querySelector('.outerDiv:nth-child(1)').style.background = 'url(chik.jfif)'
 }
 
 function shawarma(){
     document.querySelector('body').style.backgroundColor = 'lightpink'
+    document.querySelector('.outerDiv:nth-child(2)').style.backgroundImage = 'url(shawarma.jfif)'
 }
 
 function berbecue(){
     document.querySelector('body').style.backgroundColor = "lightsalmon"
+    document.querySelector('.outerDiv:nth-child(3)').style.backgroundImage = 'url(barb.jfif)'
 }
